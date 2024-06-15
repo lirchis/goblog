a7becc36c81b1c36f2df9d9a5f7a6a98087bf430
lirchis <2955908973@qq.com>
2024-06-15 20:14:51 +0800

    finish

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	conf "github.com/lirchis/goblog/configs"
+	"github.com/lirchis/goblog/internal/repo"
+	"github.com/lirchis/goblog/internal/router"
+	"github.com/lirchis/goblog/internal/utils"
+	"github.com/zxysilent/logs"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Printf("hello world")
+	ctx := logs.TraceCtx(context.Background())
+	defer logs.Close()
+	logs.SetText()
+	logs.Ctx(ctx).Info("app initializing")
+	conf.Init()
+	repo.Init(ctx)
+	defer repo.Close()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	logs.Info("app running")
+	if !utils.FileExist("./static") {
+		os.MkdirAll("./static", 0755)
+	}
+	if !utils.FileExist("./dist") {
+		os.MkdirAll("./dist", 0755)
+	}
+	app := router.Init()
+	// json.NewEncoder(os.Stdout).Encode(app.Routes())
+	go func() {
+		if err := app.Start(conf.Addr()); err != nil {
+			if err != http.ErrServerClosed { //Normal exit
+				logs.Ctx(ctx).Error("Server Shutdown:", err)
+			}
+		}
+	}()
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := app.Shutdown(ctx); err != nil {
+		if err != http.ErrServerClosed { //Normal exit
+			logs.Ctx(ctx).Error("Server Shutdown:", err)
+		}
+	}
+	logs.Ctx(ctx).Info("app quitted")
 }
